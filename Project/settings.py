cf133f90926d5fb3ff3e9baa722e109257184139
amine <108814041+amxnese@users.noreply.github.com>
2024-04-01 11:08:19 +0200

    Changed the project databse from sqlite3 to postgreSql

--- Project/settings.py
+++ Project/settings.py
@@ -82,8 +82,11 @@ WSGI_APPLICATION = 'Project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'LMF',
+        'USER': 'postgres',
+        'PASSWORD': 'aminesed',
+        'HOST': 'localhost'
     }
 }
 
