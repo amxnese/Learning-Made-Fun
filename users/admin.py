ddff3afc9813bdacab6b9ea782f1cf3761fc97d5
amine <108814041+amxnese@users.noreply.github.com>
2024-04-02 09:17:30 +0200

    registered the new custumised User model

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-from .models import Member
-admin.site.register(Member)
+# from .models import Member
+# admin.site.register(Member)
+
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    # Define the fields to be displayed in the user list
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
+
+# Register the CustomUser model with the CustomUserAdmin
+admin.site.register(CustomUser, CustomUserAdmin)
