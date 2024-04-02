e659263b69c636dc333b3349a880738f2831f7b4
amine <108814041+amxnese@users.noreply.github.com>
2024-04-02 09:20:43 +0200

    created the login and logout functions and modified the register function

--- users/views.py
+++ users/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
-from .forms import MemberForm
 from django.contrib import messages
-from .models import Member
-from django.contrib.auth.models import User
-# from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate,login as log, logout as out
+from .models import CustomUser
 
 def login(request):
-  return render(request, 'users/login.html')
+  if request.method == "POST":
+    username = request.POST['username']
+    password = request.POST['password']
+    if not CustomUser.objects.filter(username=username):
+      messages.error(request, f"{username} is not registered")
+      return render(request, 'users/login.html')
+    user = authenticate(request, username=username, password=password)
+    if user:
+      log(request, user)
+      messages.success(request, "You have successfully logged in")
+      return render(request, 'app/home.html')
+    else:
+      messages.error(request, f"wrong password for {username}")
+      return render(request, 'users/login.html')
+  else:
+    return render(request, 'users/login.html')
 
 def register(request):
   if request.method == "POST":
@@ -16,14 +29,18 @@ def register(request):
     if password != confirm:
       messages.error(request, "passwords don't match")
       return render(request, 'users/register.html')
-    if User.objects.filter(username=username):
+    if CustomUser.objects.filter(username=username):
       messages.error(request, "username not available")
       return render(request, 'users/register.html')
-    user = User.objects.create(username=username, password=password)
-    # form = MemberForm(request.POST)
-    # if form.is_valid():
-    #   # form.save()
+    user = CustomUser.objects.create_user(username=username, password=password)
+    user1 = authenticate(request, username=username, password=password)
+    if user1:
+      log(request, user)
     messages.success(request, f"account for {username} was created successfully")
     return render(request, 'app/home.html')
   else:
     return render(request, 'users/register.html')
+
+def logout(request):
+  out(request)
+  return render(request, 'users/logout.html')
