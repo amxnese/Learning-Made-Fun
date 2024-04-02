f77b5768513e1a411dbb4156e13a0e52048a1585
amine <108814041+amxnese@users.noreply.github.com>
2024-04-02 09:19:21 +0200

    deleted the Member model and added CustomUser model

--- users/models.py
+++ users/models.py
@@ -1,13 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
-class Member(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    # username = models.CharField(max_length=100)
-    # password = models.CharField(max_length=100)
+class CustomUser(AbstractUser):
     python = models.IntegerField(default=0)
     java = models.IntegerField(default=0)
     javaScript = models.IntegerField(default=0)
-
-    def __str__(self):
-        return self.username
