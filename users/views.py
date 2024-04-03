3088c9af86906c638c318ef84d19cdd2a0cfbf6f
amine <108814041+amxnese@users.noreply.github.com>
2024-04-03 11:01:24 +0100

    added profile, edit, change_password and delete functions to the users/views.py file

--- users/views.py
+++ users/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import authenticate,login as log, logout as out
 from .models import CustomUser
+from django.contrib.auth.decorators import login_required
 
 def login(request):
   if request.method == "POST":
@@ -41,6 +42,64 @@ def register(request):
   else:
     return render(request, 'users/register.html')
 
+@login_required
 def logout(request):
   out(request)
   return render(request, 'users/logout.html')
+
+@login_required
+def profile(request):
+  user = request.user
+  python = user.python * 25
+  java = user.java * 25
+  javaScript = user.javaScript * 25
+  context = {
+    'python' : python,
+    'java' : java,
+    'javaScript' : javaScript
+  }
+  return render(request, 'users/profile.html',{'lvl':context})
+
+@login_required
+def edit(request):
+  if request.method == 'POST':
+    user = request.user
+    username = request.POST.get('username')
+    fname = request.POST.get('fname')
+    lname = request.POST.get('lname')
+    email = request.POST.get('email')
+    user.username = username if username else user.username
+    user.first_name = fname if fname else user.first_name
+    user.last_name = lname if lname else user.last_name
+    user.email = email if email else user.email
+    user.save()
+    messages.success(request, 'Profile updated successfully')
+    return render(request, 'app/home.html')
+  else:
+    return render(request, 'users/edit.html')
+
+@login_required
+def change_password(request):
+  if request.method == 'POST':
+    user = request.user
+    old = request.POST.get('old')
+    new = request.POST.get('new')
+    confirm = request.POST.get('confirm')
+    if not user.check_password(old):
+      messages.error(request, 'Wrong password!')
+      return render(request, 'users/passwd.html')
+    if new != confirm:
+      messages.error(request, "Passwords don't match")
+      return render(request, 'users/passwd.html')
+    user.set_password(new)
+    user.save()
+    messages.success(request, 'Updated password successfully')
+    return render(request, 'app/home.html')
+  else:
+    return render(request, 'users/passwd.html')
+
+@login_required
+def delete(request):
+  user = request.user
+  user.delete()
+  return render(request, 'users/delete.html')
