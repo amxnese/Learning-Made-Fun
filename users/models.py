a0258929cf4219b5b009c8a969ba85cd1ce7f4b5
amine <108814041+amxnese@users.noreply.github.com>
2024-04-08 17:45:31 +0100

    added the profile picture field to the database

--- users/models.py
+++ users/models.py
@@ -5,3 +5,4 @@ class CustomUser(AbstractUser):
     python = models.IntegerField(default=0)
     java = models.IntegerField(default=0)
     javaScript = models.IntegerField(default=0)
+    profile_picture = models.ImageField(null=True, blank=True, upload_to='images/')
