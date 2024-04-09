a7a5494dba33b33968d5ba51fba123d983de93de
amine <108814041+amxnese@users.noreply.github.com>
2024-04-09 09:29:03 +0100

    modified the upload_to path and set the default to 0 in the imagefield

--- users/models.py
+++ users/models.py
@@ -5,4 +5,4 @@ class CustomUser(AbstractUser):
     python = models.IntegerField(default=0)
     java = models.IntegerField(default=0)
     javaScript = models.IntegerField(default=0)
-    profile_picture = models.ImageField(null=True, blank=True, upload_to='images/')
+    profile_picture = models.ImageField(null=True, blank=True, upload_to='', default='')
