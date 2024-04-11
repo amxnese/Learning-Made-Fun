9c587ce86dc0c096ad8c0932c8db330fc4ec5b16
amine <108814041+amxnese@users.noreply.github.com>
2024-04-11 12:47:51 +0100

    added main function to views

--- app/views.py
+++ app/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
 def home(request):
   return render(request, 'app/home.html')
+
+def main(request):
+  return render(request, 'app/main.html')
 
 @login_required
 def python(request):
