b6341ebb8b3cebc6aa1fba50069df4082eecc84e
amine <108814041+amxnese@users.noreply.github.com>
2024-04-19 21:28:51 +0100

    imported bootstrap's alerts and changed the css style so bootstrap don't affect it and modified the way that hide.js hides messages

--- app/static/app/hide.js
+++ app/static/app/hide.js
@@ -1,4 +1,14 @@
-let msg = document.getElementsByClassName("alert")[0];
-setTimeout(() => {
+function hideMessage() {
+  let msg = document.querySelector(".alert");
+  setTimeout(() => {
+    msg.style.opacity = '0';
+  }, 2300);
+  setTimeout(() => {
+    msg.style.display = "none";
+  }, 3000);
+}
+let closed = document.querySelector(".close-button");
+closed.addEventListener("click", () => {
+  let msg = document.querySelector(".alert");
   msg.style.display = "none";
-}, 3000);
+});
