7fa4f6ff2980cca0f3489ab091a1b0959caed527
amine <108814041+amxnese@users.noreply.github.com>
2024-05-18 19:06:46 +0100

    created users/msg.css file for the confirmation modal for deleting an account and modified users/del-btn.js to match new updates

--- users/static/users/del-btn.js
+++ users/static/users/del-btn.js
@@ -1,15 +1,12 @@
 document.getElementById("deleteButton").addEventListener("click", function () {
-  document.getElementById("confirmationModal").style.display = "block";
+  document.getElementById("modal").style.display = "block";
 });
 
-document.getElementById("closeButton").addEventListener("click", function () {
-  document.getElementById("confirmationModal").style.display = "none";
-});
-
-document.getElementById("confirmDelete").addEventListener("click", function () {
+document.getElementById("confirm").addEventListener("click", function () {
   window.location.href = "delete/";
 });
 
-function change_color(){
-  document.getElementById("confirmDelete").style.backgroundColor = "#ff4d4f"
-}
+document.getElementById("cancel").addEventListener("click", function () {
+  let msg = document.getElementById("modal");
+  msg.style.display = "none";
+});
