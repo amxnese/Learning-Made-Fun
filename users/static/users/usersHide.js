376f90d87095da792c8a8c5bc1d0b87073dd539b
amine <108814041+amxnese@users.noreply.github.com>
2024-05-18 19:11:52 +0100

    modified the users/usersHide.js file

--- users/static/users/usersHide.js
+++ users/static/users/usersHide.js
@@ -1,9 +1,15 @@
 function hideMessage() {
   let msg = document.querySelector(".alert");
   setTimeout(() => {
-    msg.style.opacity = '0';
+    msg.style.opacity = "0";
   }, 1700);
   setTimeout(() => {
     msg.style.display = "none";
-  }, 4000);
+  }, 3000);
+}
+
+function closeMe() {
+  console.log("hi");
+  let msg = document.getElementById("errorMessage");
+  msg.style.display = "none";
 }
