e1adcc019ed1e2cec417ed134a00b0bd51161a6f
amine <108814041+amxnese@users.noreply.github.com>
2024-05-21 22:07:34 +0100

    created a new database table (comments)

--- users/models.py
+++ users/models.py
@@ -1,8 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
+from django.conf import settings
 class CustomUser(AbstractUser):
     python = models.IntegerField(default=0)
     java = models.IntegerField(default=0)
     javaScript = models.IntegerField(default=0)
     profile_picture = models.ImageField(null=True, blank=True, upload_to='', default='')
+
+class Comment(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    def __str__(self):
+        return f"Comment by {self.user.username} on {self.created_at}"
