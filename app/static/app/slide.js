a36b3b68a0797241b4f3a1eecad8f393de8941fd
amine <108814041+amxnese@users.noreply.github.com>
2024-05-31 02:37:56 +0100

    fixed some bugs and set an interval so that the comments slider slide automatically

--- app/static/app/slide.js
+++ app/static/app/slide.js
@@ -3,6 +3,9 @@ let prev = document.querySelector(".prev");
 let slider = document.querySelector(".slider");
 next.addEventListener('click', function() {
   let slides = document.querySelectorAll('.slide');
+  if(slides.length == 0){
+    return
+  }
   let slidesArr = Array.from(slides);
   let active = slidesArr.findIndex(slide => slide.classList.contains('active'))
   if(active+1 < slides.length){
@@ -10,6 +13,11 @@ next.addEventListener('click', function() {
     slides[active+1].classList.add('active')
     console.log(active);
     slider.style.transform = `translateX(${-700*(active+1)}px)`;
+  }
+  else{
+    slides[active].classList.remove('active')
+    slides[0].classList.add('active')
+    slider.style.transform = `translateX(0px)`;
   }
 })
 prev.addEventListener('click', function() {
@@ -23,3 +31,6 @@ prev.addEventListener('click', function() {
     slider.style.transform = `translateX(${-700*(active-1)}px)`;
   }
 })
+setInterval(() => {
+  next.click()
+}, 5000);
