f6f7e58a31cbcd76e3348e787871834e92a71a7f
amine <108814041+amxnese@users.noreply.github.com>
2024-06-04 00:01:50 +0100

    handled the go back arrow logic

--- users/static/users/change-pic.js
+++ users/static/users/change-pic.js
@@ -1,7 +1,12 @@
 let input = document.querySelector(".file-input");
 let to_hide = document.querySelector(".to-hide");
-input.addEventListener('change', function(event) {
+input.addEventListener("change", function (event) {
   if (event.target.files.length > 0) {
-    to_hide.click()
+    to_hide.click();
   }
+});
+
+let icon = document.querySelector(".arrow");
+icon.addEventListener("click", () => {
+  location.href = "../";
 });
