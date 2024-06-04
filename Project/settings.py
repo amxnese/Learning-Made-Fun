9882ef8999a30b2e6521e8d44d7de897f8f92167
amine <108814041+amxnese@users.noreply.github.com>
2024-06-04 18:00:51 +0100

    configured the static files

--- Project/settings.py
+++ Project/settings.py
@@ -21,9 +21,11 @@ STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static')
 ]
 
+
 # Media files
 MEDIA_URL = 'users/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'users/media')
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -130,7 +132,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # STATICFILES_DIRS = [
 #     BASE_DIR / "static",
@@ -145,3 +147,5 @@ AUTH_USER_MODEL = 'users.CustomUser'
 
 #default login page
 LOGIN_URL = '/login/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
