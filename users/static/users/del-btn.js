6e8aec8cafba4888ee586febb7a98560c5ad1aba
amine <108814041+amxnese@users.noreply.github.com>
2024-06-19 18:02:20 +0100

    modified the del-btn.js and profile.css and profile.html to add the effect of making the background darker when a pop up pops up in the profile page

--- users/static/users/del-btn.js
+++ users/static/users/del-btn.js
@@ -1,12 +1,14 @@
 document.getElementById("deleteButton").addEventListener("click", function () {
   document.getElementById("modal").style.display = "block";
+  document.getElementById("overlay").style.display = "block";
 });
 
 document.getElementById("confirm").addEventListener("click", function () {
   window.location.href = "delete/";
+  document.getElementById("overlay").style.display = "none";
 });
 
 document.getElementById("cancel").addEventListener("click", function () {
-  let msg = document.getElementById("modal");
-  msg.style.display = "none";
+  document.getElementById("modal").style.display = "none";
+  document.getElementById("overlay").style.display = "none";
 });
